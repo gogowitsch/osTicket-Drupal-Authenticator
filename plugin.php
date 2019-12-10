2ec77b0b368adaea9ca20b3a05be149adaaca0c8
Christian Bläul <blaeul@quodata.de>
2019-12-10 12:19:43 +0100

    Renamed file to silence autoload warning

--- plugin.php
+++ plugin.php
@@ -7,6 +7,6 @@ return array(
     'description' =>    /* trans */ 'Provides a configurable authentication
                          backend for authenticating staff and clients using Drupal 8.',
     'url' =>            'https://github.com/Fonata/osTicket-Drupal-Authenticator',
-    'plugin' =>         'authentication.php:DrupalAuthPlugin',
+    'plugin' =>         'DrupalAuthPlugin.php:DrupalAuthPlugin',
     'requires' => array()
 );
